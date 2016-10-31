b6bf8bd2b10ff304f791ea104f1081745fcf0084
danswick <dan.swick@gmail.com>
2016-10-31 09:42:44 -0700

    add sesh 4

---
+++ fall2016/session-4/pull-request-map/js/names.js
@@ -0,0 +1,13 @@
+/* Add your name and a location below. It should look like:
+ *
+ * ["name", [lon, lat]]
+ *
+ * Don't forget to check your commas! 
+ *
+ */
+
+var locations = [
+  ["CCSFGIS Class", [-122.420, 37.755]],
+  ["Sausalito Bay Model", [-122.4949,37.8635]],
+  ["Presidio Sculpture thing", [-122.4600,37.7919]]
+];
